17ddffd8943c9c97e62928f74a563637752a8316
idncod <idncod@protonmail.com>
2024-09-30 22:37:37 +0100

    fix port for index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use (express.json());
 
 app.use(routes);
 
-const PORT = 'https://rightmove-challenge.onrender.com' || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on: http://localhost:${PORT}`);
 });
