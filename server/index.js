610d25fdc72092a15e5bc432d52a4135f044a7c3
idncod <idncod@protonmail.com>
2024-09-30 22:48:53 +0100

    fix deploy backend with render

--- server/index.js
+++ server/index.js
@@ -5,12 +5,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
-app.use (express.json());
-
+app.use(express.json());
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on port: ${PORT}`);
 });
