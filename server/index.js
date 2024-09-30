e473d71536052a6e513e9fb80001dadf97530572
idncod <idncod@protonmail.com>
2024-09-30 23:32:42 +0100

    revert index.js back to exisitng port

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Listening on: http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log('Listening on: localhost:3000');
 });
